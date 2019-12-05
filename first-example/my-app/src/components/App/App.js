6ad88253164f05ae72a13998de184657c121e383
evanboissonnot <evan@boissonnot.fr>
2019-12-05 17:56:42 +0100

    one root element by component

--- first-example/my-app/src/components/App/App.js
+++ first-example/my-app/src/components/App/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a react app</h1>
-    //   </div>
-    // );
-    return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello'));
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react app</h1>
+      </div>
+      <h1>
+        Pas possible
+      </h1>
+    );
   }
   
 } 
