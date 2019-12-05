1c441e10559e0b3369e8e74c0081a59dab6da412
evanboissonnot <evan@boissonnot.fr>
2019-12-05 18:10:25 +0100

    adding an other component

--- first-example/my-app/src/components/App/App.js
+++ first-example/my-app/src/components/App/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from '../Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
+        <p>
+          Working !
+        </p>
+        <Person />
+        <Person></Person>
       </div>
-      <h1>
-        Pas possible
-      </h1>
     );
   }
-  
-} 
+
+}
 
 export default App;
