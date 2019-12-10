72cebc52e070d7e09337387b43331f67efa498da
evanboissonnot <evan@boissonnot.fr>
2019-12-10 12:18:13 +0100

    two way is done

--- first-example/my-app/src/components/People/People-1.js
+++ first-example/my-app/src/components/People/People-1.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const style = {
+    backgroundColor: 'grey',
+    margin: '20px'
+};
+
 const People1 = (props) => {
     return (
-        <div>
+        <div style={ style }>
             <b>People !</b>
             <span>{props.name}</span>
+            <p>
+                <input type="text" value={props.name} onChange={props.update}></input>
+            </p>
             <p>
                 <button onClick={props.delete}>Delete</button>
             </p>
--- first-example/my-app/src/components/Person/Person-Iteration.js
+++ first-example/my-app/src/components/Person/Person-Iteration.js
@@ -4,16 +4,16 @@ import People from '../People/People-1';
 class PersonIteration extends Component {
     state = {
         people: [
-            { name: 'kangourou' },
-            { name: 'jenki' },
-            { name: 'couwi' },
+            { key: 1, name: 'kangourou' },
+            { key: 10, name: 'jenki' },
+            { key: 58, name: 'couwi' },
         ]
     };
 
     deleteOnPeople = (index) => {
         console.log(index);
 
-        const people = this.state.people;
+        const people = [...this.state.people];
         people.splice(index, 1);
 
         this.setState({
@@ -21,16 +21,41 @@ class PersonIteration extends Component {
         });
     }
 
+    updateOnPeople = (event, personKey) => {
+        console.log('personKey', personKey);
+        console.log('event', event);
+
+        //const onePeople = { ...this.state.people.find(item => item.key === personKey) };
+        const peopleIndex = this.state.people.findIndex(item => item.key === personKey);
+        const newOnePeople = {...this.state.people[peopleIndex]};
+
+        newOnePeople.name = event.target.value;
+
+        const newPeople = [...this.state.people];
+        newPeople[peopleIndex] = newOnePeople;
+
+        this.setState({
+            people: newPeople
+        });
+    }
+
     render() {
+        const style = {
+            display: 'flex'
+        };
+
         return (
-            <div>
+            <div style={style}>
                 <div>
                     <b>PersonConditional</b>
                 </div>
                 {
                     this.state.people.map((item, index) => {
                         // return <div>{ item.name }</div>
-                        return <People name={item.name} delete={ this.deleteOnPeople.bind(this, index)}></People>
+                        return <People name={item.name} 
+                                       delete={ this.deleteOnPeople.bind(this, index)} 
+                                       update={ (event) => this.updateOnPeople(event, item.key) }
+                                       key={item.key}></People>
                     })
                 }
             </div>
