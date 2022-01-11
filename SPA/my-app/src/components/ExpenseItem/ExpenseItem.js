143bec3d259aea562bf33b9df5b0043d8bec2047
Evan BOISSONNOT <evan@boissonnot.fr>
2022-01-11 18:05:26 +0100

    feat(component): passing data to props

--- SPA/my-app/src/components/ExpenseItem/ExpenseItem.js
+++ SPA/my-app/src/components/ExpenseItem/ExpenseItem.js
@@ -1,14 +1,12 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const expenseDate = new Date();
-    const expenseTitle = 'Dépense';
-    const amount = 250.20;
+    const {title, amount, date} = props.data;
 
     return (
         <div className='expense-item'>
-            <div >{expenseDate.toLocaleString()}</div>
-            <h2 className='expense-item__description'>{expenseTitle}</h2>
+            <div >{date.toLocaleString()}</div>
+            <h2 className='expense-item__description'>{title}</h2>
             <div>{amount} €</div>
         </div>
     );
