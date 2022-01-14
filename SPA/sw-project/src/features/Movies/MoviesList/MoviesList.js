65ad8bfd613e75188cd5fd16424cacc48100b45c
Evan BOISSONNOT <evan@boissonnot.fr>
2022-01-14 17:32:29 +0100

    feat(movies): display spinner

--- SPA/sw-project/src/features/Movies/MoviesList/MoviesList.js
+++ SPA/sw-project/src/features/Movies/MoviesList/MoviesList.js
@@ -5,15 +5,22 @@ import MoviesService from "../Services/MoviesService";
 
 const MoviesList = props => {
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     const loadMovies = async (event) => {
+        setIsLoading(true);
+
         const movies = await (new MoviesService()).getAll()
+        setIsLoading(false);
         setMovies(previousList => movies);
     };
+
+    const spinner = isLoading ? <i>Data loading ....</i> : null;
 
     return (
         <div>
             <button onClick={ loadMovies }>Load</button>
+            {spinner}
             <MoviesTable movies={movies}></MoviesTable>
         </div>
     );
