1afc9c142f74fbbf54c4d24376430a3809a9725a
Evan BOISSONNOT <evan@boissonnot.fr>
2022-01-19 09:08:40 +0100

    feat(api): add one

---
+++ api/webapi dotnet/SuiviDrone.Web.API.UI/SuiviDrone.Web.API.UI/Controllers/DroneController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SuiviDrones.Interfaces.Repositories;
+
+namespace SuiviDrone.Web.API.UI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    [Authorize]
+    public class DroneController : ControllerBase
+    {
+        #region Fields
+        private readonly IDroneRepository repository = null!;
+        #endregion
+
+        #region Constructors
+        public DroneController(IDroneRepository repository)
+        {
+            this.repository = repository;
+        }
+        #endregion
+
+        #region Public methods
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return this.Ok(this.repository.GetList());
+        }
+        #endregion
+    }
+}
