f0db70c3a54e6a5eb2e6c67bf5aa814fa9f2f6cf
Evan BOISSONNOT <evan@boissonnot.fr>
2022-01-19 23:17:07 +0100

    feat(auth): update auth

--- SPA/suivi-drones/src/containers/App/App.js
+++ SPA/suivi-drones/src/containers/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Authentication from '../../features/users/Authentication/Authentication';
+import Authentication from '../../features/users/authentication/authentication';
+import Header from '../../shared/components/header';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <header className="App-header">
+        <Header></Header>
       </header>
       <Authentication></Authentication>
     </div>
--- SPA/suivi-drones/src/features/users/Authentication/Authentication.js
+++ SPA/suivi-drones/src/features/users/Authentication/Authentication.js
@@ -1,15 +1,39 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 import AuthenticationService from './auth-service';
 
 const Authentication = props => {
+    const [userInput, setUserInput] = useState({
+        login: '',
+        password: ''
+    });
+
     const logUser = async () => {
-        const user = await AuthenticationService.login('', '');
+        console.info('log', null);
+        const user = await AuthenticationService.login(userInput.login, userInput.password);
+    }
+
+    const loginChangedHandler = (event) => {
+        setUserInput(prevState => ({ ...prevState, login: event.target.value }));
+    }
+    const passwordChangedHandler = (event) => {
+        setUserInput(prevState => ({ ...prevState, password: event.target.value }));
     }
 
     return (
-        <div className="row">
-            <div className="col">
-                <button className="button success" onClick={ logUser }>Login</button>
+        <div className="container">
+            <div className="row">
+                <div className="col">Email</div>
+                <div className="col"><input type="email" value={userInput.login} onChange={loginChangedHandler}></input></div>
+            </div>
+            <div className="row">
+                <div className="col">Password</div>
+                <div className="col"><input type="password" value={userInput.password} onChange={passwordChangedHandler}></input></div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <button className="button success" onClick={logUser}>Login</button>
+                </div>
             </div>
         </div>
     );
