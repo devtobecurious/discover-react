dad2e5761e0b0712b13dd312827c79354deb488c
Evan BOISSONNOT <evan@boissonnot.fr>
2022-01-28 18:24:46 +0100

    test: use async

--- SPA/sw-project/src/features/Planets/PlanetTable/PlanetTable.js
+++ SPA/sw-project/src/features/Planets/PlanetTable/PlanetTable.js
@@ -1,11 +1,18 @@
+import { useEffect, useState } from "react";
+
 const PlanetTable = props => {
+    const [planets, setPlanets] = useState([]);
+
+    useEffect(() => {
+        fetch('https://swapi.dev/api/planets').then(resp => resp.json())
+                                              .then(data => setPlanets(data.results));
+    })
+
     return (
         <>
             <table>
                 <tbody>
-                    <tr>
-                        <td></td>
-                    </tr>
+                    { planets.forEach(item => <tr key={item.name}><td>{item.name}</td></tr>) }
                 </tbody>
             </table>
         </>
--- SPA/sw-project/src/features/Planets/PlanetTable/PlanetTable.test.js
+++ SPA/sw-project/src/features/Planets/PlanetTable/PlanetTable.test.js
@@ -1,12 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import PlanetTable from "./PlanetTable";
 
-test('display 2 planets', () => {
-    render(<PlanetTable></PlanetTable>);
+describe('Planetes', () => {
+    test('display 2 planets', () => {
+        render(<PlanetTable></PlanetTable>);
 
-    const table = screen.queryByRole("table");
-    console.log(table.rows.length);
+        const table = screen.queryByRole("table");
+        console.log(table.rows.length);
 
-    expect(table).toBeInTheDocument(table);
-    expect(table.rows.length).toEqual(1);
+        expect(table).toBeInTheDocument(table);
+        expect(table.rows.length).toEqual(1);
+    })
+
+    test('call fetch', async () => {
+        render(<PlanetTable></PlanetTable>);
+
+        const rows = await screen.findAllByRole('row');
+        expect(rows).not.toHaveLength(0);
+    });
 });
