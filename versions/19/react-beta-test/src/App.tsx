9d98b98a139e2d259c4f7f7058171342643d3ea8
Evan BOISSONNOT <evan@boissonnot.fr>
2024-08-20 18:47:04 +0200

    core: prepare to rc 19

---
+++ versions/19/react-beta-test/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { UpdateName } from './features/users/components/UpdateName'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <UpdateName></UpdateName>
+    </>
+  )
+}
+
+export default App
